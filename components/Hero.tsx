d976242d8a49c67c754fd015dbdd6e92504b7acd
princedalsaniya <prince.d@ahduni.edu.in>
2023-07-09 02:22:19 +0530

    Added this : - Footer - Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, Book, Rent a car - quickly and easily!
+          Find, Book, Rent a car — quickly and easily!
         </h1>
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking process.

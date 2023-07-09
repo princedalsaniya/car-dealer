9437f9af9dbe52bebb13dac5b66f85954335e590
princedalsaniya <prince.d@ahduni.edu.in>
2023-07-09 12:05:47 +0530

    Added this : - Headless UI package - Combobox in SearchManufracturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,16 +3,44 @@
   Description : This is searchbar for manufracturer. It has autocomplete feature.
   Params      : manufracturer     =>  manufracturer state
                 setManufracturer  =>  setter function for manufraturer state
-  Requires    : None
+  Requires    : ComboBox => To make searchbar autoComplete
   Author      : Prince Dalsaniya
 */
 
+'use client'
+
 // Imports
 import { SearchManufracturerProps } from "@/types"
+import { Combobox, Transition } from "@headlessui/react"
+import { useState } from "react"
+import Image from "next/image"
 
 const SearchManufacturer = ({ manufracturer, setManufracturer } : SearchManufracturerProps) => {
+  const [query, setQuery] = useState('')
+
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufracturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className='absolute top-[14px]'>
+            <Image
+              src='/car-logo.svg'
+              width={20}
+              height={20}
+              className='ml-4'
+              alt='car logo'
+            />
+          </Combobox.Button>
+
+          <Combobox.Input 
+            className='search-manufacturer__input' 
+            placeholder="Wolkswagen"
+            displayValue={(manufracturer: string) => manufracturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </Combobox>
+    </div>
   )
 }
 

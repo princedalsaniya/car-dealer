5f3e0e639020a45196a0d9c1f76175209fb333b6
princedalsaniya <prince.d@ahduni.edu.in>
2023-07-23 19:39:39 +0530

    Added this : - Initiated CarDetails component - Used it in carCard and passed values required

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -15,7 +15,7 @@ import Image from "next/image"
 import { CarProps } from "@/types";
 import CustomBtn from "./CustomBtn";
 import { calculateCarRent } from "@/utils";
-// import CarDetails from "./CarDetails";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -86,7 +86,7 @@ function CarCard({ car }: CarCardProps) {
         </div>
       </div>
 
-      {/* <CarDetails isOpen={isOpen} closeModel={() => setIsOpen(false)} car={car}/> */}
+      <CarDetails isOpen={isOpen} closeModel={() => setIsOpen(false)} car={car}/>
     </div>
   )
 }
--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,4 +1,12 @@
-const CarDetails = () => {
+import { CarProps } from "@/types";
+
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModel: () => void;
+  car: CarProps;
+}
+
+const CarDetails = ({ isOpen, closeModel, car } : CarDetailsProps) => {
   return (
     <div>CarDetails</div>
   )

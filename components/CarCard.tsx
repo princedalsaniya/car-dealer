7f1cb66a8101b60fa03e39d276aad84a4de872e6
princedalsaniya <prince.d@ahduni.edu.in>
2023-07-23 20:15:14 +0530

    Added This : - Added class to handle car image in small screensize - Added a transition - Opening an empty modal on click

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -48,7 +48,7 @@ function CarCard({ car }: CarCardProps) {
       </p>
 
       <div className="relative w-full h-40 my-3 object-contain">
-        <Image src={'/hero.png'} alt="car model" fill priority/>
+        <Image src={'/hero.png'} alt='car model' fill priority className='object-contain' />
       </div>
 
       <div className="relative flex w-full mt-2">
@@ -86,7 +86,7 @@ function CarCard({ car }: CarCardProps) {
         </div>
       </div>
 
-      <CarDetails isOpen={isOpen} closeModel={() => setIsOpen(false)} car={car}/>
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/>
     </div>
   )
 }
